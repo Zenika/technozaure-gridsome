177d4768c22c481b1554677c23db17cb3807ab75
Thomas Betous <tbetous@gmail.com>
2020-02-08 19:44:39 +0100

    :tada: Initial commit
    Initial commit

---
+++ gridsome.config.js
@@ -0,0 +1,34 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: 'gridsome-source-conference-hall',
+      options: {
+        apiKey: process.env.CONFERENCE_HALL_API_KEY,
+        eventId: process.env.CONFERENCE_HALL_EVENT_ID,
+        filterConfirmedTalks: true
+      }
+    },
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true,
+        shouldPurgeUnusedKeyframes: true
+      }
+    }
+  ],
+  templates: {
+    Speaker: '/speaker/:id',
+    Talk: '/talk/:id'
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import DefaultLayout from './layouts/Default.vue'
+
+export default function(Vue) {
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout)
+}
