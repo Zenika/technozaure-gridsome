b2db7c46358d12e7c040fe8ddb60708dd3d3e9d7
tbetous <tbetous@gmail.com>
2020-02-24 08:32:08 +0100

    :recycle: Externalize talk card

---
+++ src/components/TalkCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <g-link :to="talk.path">
+    <article class="talk-card flex flex-col h-full p-4">
+      <header class="flex h-16 mb-4 items-center justify-center">
+        <h2 class="text-lg text-center lg:text-xl truncate-2-lines">
+          {{ talk.title }}
+        </h2>
+      </header>
+      <section class="flex-1">
+        <ul class="flex flex-wrap justify-center mb-4">
+          <li v-if="talk.formats" class="p-1 mt-1 text-xs border rounded-md">
+            {{ talk.formats.name }}
+          </li>
+          <li
+            v-if="talk.categories"
+            class="p-1 ml-1 mt-1 text-xs border rounded-md"
+          >
+            {{ talk.categories.name }}
+          </li>
+        </ul>
+      </section>
+      <section class="px-4 overflow-hidden">
+        <ul class="flex flex-wrap flex-row justify-center">
+          <li v-for="speaker in talk.speakers" :key="speaker.id" class="mx-2">
+            <div class="flex flex-col items-center justify-center">
+              <g-image class="w-10 rounded-full" :src="speaker.photoURL" />
+              <div
+                class="flex flex-col flex-1 items-center justify-center truncate"
+              >
+                <span class="text-sm text-center">{{
+                  speaker.displayName
+                }}</span>
+                <span class="text-xs text-gray-600 text-center flex-1">{{
+                  speaker.company
+                }}</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </article>
+  </g-link>
+</template>
+
+<script>
+export default {
+  name: 'TalkCard',
+  props: {
+    talk: Object
+  }
+}
+</script>
--- src/pages/Talks.vue
+++ src/pages/Talks.vue
@@ -5,57 +5,9 @@
         <li
           v-for="talk in talks"
           :key="talk.id"
-          class="border-2 border-gray-500 shadow-lg p-4 "
+          class="border-2 border-gray-500 shadow-lg"
         >
-          <article class="flex flex-col h-full">
-            <header class="flex h-16 mb-4 items-center justify-center">
-              <h2 class="text-lg text-center lg:text-xl truncate-2-lines">
-                {{ talk.title }}
-              </h2>
-            </header>
-            <section class="flex-1">
-              <ul class="flex flex-wrap justify-center mb-4">
-                <li
-                  v-if="talk.formats"
-                  class="p-1 mt-1 text-xs border rounded-md"
-                >
-                  {{ talk.formats.name }}
-                </li>
-                <li
-                  v-if="talk.categories"
-                  class="p-1 ml-1 mt-1 text-xs border rounded-md"
-                >
-                  {{ talk.categories.name }}
-                </li>
-              </ul>
-            </section>
-            <section class="px-4 overflow-hidden">
-              <ul class="flex flex-wrap flex-row justify-center">
-                <li
-                  v-for="speaker in talk.speakers"
-                  :key="speaker.id"
-                  class="mx-2"
-                >
-                  <div class="flex flex-col items-center justify-center">
-                    <g-image
-                      class="w-10 rounded-full"
-                      :src="speaker.photoURL"
-                    />
-                    <div
-                      class="flex flex-col flex-1 items-center justify-center truncate"
-                    >
-                      <span class="text-sm text-center">{{
-                        speaker.displayName
-                      }}</span>
-                      <span class="text-xs text-gray-600 text-center flex-1">{{
-                        speaker.company
-                      }}</span>
-                    </div>
-                  </div>
-                </li>
-              </ul>
-            </section>
-          </article>
+          <talk-card :talk="talk"></talk-card>
         </li>
       </ul>
     </div>
@@ -87,8 +39,13 @@ query {
 </page-query>
 
 <script>
+import TalkCard from '~/components/TalkCard.vue'
+
 export default {
   name: 'Speakers',
+  components: {
+    TalkCard
+  },
   computed: {
     talks() {
       return this.$page.allTalk.edges.map(edge => edge.node)
