335eab1730ddee647e8d561aae223c7985dbf6ff
TarekTouati <209480@supinfo.com>
2020-03-05 13:15:09 +0100

    :sparkles: Speaker Cards
    Add speaker cards

---
+++ src/components/SpeakerCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <g-link :to="speaker.path">
+    <article class="talk-card flex flex-col h-full p-4">
+      <header class="flex h-16 mb-4 items-center justify-center">
+        <div class="flex flex-col items-center justify-center">
+          <g-image class="w- rounded-full" :src="speaker.photoURL" />
+        </div>
+      </header>
+      <section class="px-4 overflow-hidden">
+        <h2 class="text-lg text-center lg:text-xl truncate-2-lines">
+          {{ speaker.displayName }}
+        </h2>
+        <div class="flex flex-col flex-1 items-center justify-center truncate">
+          <span class="text-xs text-gray-600 text-center flex-1">{{
+            speaker.company
+          }}</span>
+        </div>
+      </section>
+    </article>
+  </g-link>
+</template>
+
+<script>
+export default {
+  name: 'SpeakerCard',
+  props: {
+    speaker: Object
+  }
+}
+</script>
