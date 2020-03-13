1b36e8c8be4b0313d5f0b73ac0c4ffd9ee33ad4e
TarekTouati <209480@supinfo.com>
2020-03-13 17:30:36 +0100

    :lipstick: updated UI style

--- src/components/SpeakerCard.vue
+++ src/components/SpeakerCard.vue
@@ -1,15 +1,15 @@
 <template>
   <g-link :to="speaker.path">
     <article class="talk-card flex flex-col h-full p-4">
-      <header class="flex h-16 mb-4 items-center justify-center">
+      <header class="flex h-32 mb-4 items-center justify-center">
         <div class="flex flex-col items-center justify-center">
-          <g-image class="w-20 rounded-full" :src="speaker.photoURL" />
+          <g-image class="w-24 rounded-full m-3" :src="speaker.photoURL" />
+          <h2 class="text-lg text-center lg:text-xl truncate-2-lines">
+            {{ speaker.displayName }}
+          </h2>
         </div>
       </header>
       <section class="px-4 overflow-hidden">
-        <h2 class="text-lg text-center lg:text-xl truncate-2-lines">
-          {{ speaker.displayName }}
-        </h2>
         <div class="flex flex-col flex-1 items-center justify-center truncate">
           <span class="text-xs text-gray-600 text-center flex-1">{{
             speaker.company
